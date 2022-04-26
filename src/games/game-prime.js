6af0aaeddc8cee2624e3f72de797f8e41a396701
Lev Kuzmin <levnc5@gmail.com>
2022-04-26 15:49:15 +0530

    Modify isPrime() to find prime number
    Change the way to find prime number

--- src/games/game-prime.js
+++ src/games/game-prime.js
@@ -3,15 +3,29 @@ import { getRandomInt } from '../randomGenerator.js';
 
 const gameTask = 'Answer "yes" if given number is prime. Otherwise answer "no".';
 
-const primes = [2, 3, 5, 7, 11, 13, 17, 19, 23, 29, 31, 37,
-  41, 43, 47, 53, 59, 61, 67, 71, 73, 79, 83, 89, 97];
+const isPrime = (num) => {
+  if (num === 1) {
+    return false;
+  }
 
-// Predicate
-const isPrime = (num) => primes.includes(num);
+  const firstPrime = 2;
+  if (num === firstPrime) {
+    return true;
+  }
+
+  const maxDivisor = Math.floor(num / 2);
+  for (let i = 0; i <= maxDivisor - firstPrime; i += 1) {
+    const divisor = i + firstPrime;
+    if (num % divisor === 0) {
+      return false;
+    }
+  }
+  return true;
+};
 
 export const getQuestionAnswer = () => {
   const [minNum, maxNum] = [1, 100];
-  // Number is a question
+  // Question is a number
   const question = getRandomInt(minNum, maxNum);
   const answer = isPrime(question) ? 'yes' : 'no';
 
